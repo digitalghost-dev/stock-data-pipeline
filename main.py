96015d44af64e03b26e3ea19c74a0de7e21bb8cf
SquareHammer89 <pythonsanchez@gmail.com>
2022-07-19 07:15:10 -0700

    importing bq_update.py function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from config import api_key, uri
+from bq_update import *
 import pandas as pd
 import subprocess
 import requests
@@ -9,6 +10,7 @@ import time
 # Pretend to be a browser.
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36", "X-Requested-With": "XMLHttpRequest"}
 
+# Starting the program run timer.
 start_time = time.time()
 
 url_wiki = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
@@ -16,8 +18,10 @@ url_wiki = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 req = requests.get(url_wiki, headers = headers)
 table = pd.read_html(req.text, attrs = {'id':'constituents'})
 
+# Getting the symbol column from the Wikipedia table.
 first_col = table[0]['Symbol']
 
+# Putting the scraped ticker symbols in a list.
 companies = []
 for ticker in first_col:
     companies.append(ticker.lower())
@@ -40,6 +44,7 @@ def company_info():
             source = response.read()
             data = json.loads(source)
 
+        # API endpoints used.
         symbol = data["symbol"]
         company = data["companyName"]
         price = data["latestPrice"]
@@ -49,6 +54,7 @@ def company_info():
         list_of_companies.append(info)
         count += 1
         
+        # Removing commas in in company names, outputting a CSV file.
         dataframe = pd.DataFrame(list_of_companies)
         dataframe = dataframe.replace(",", "", regex=True)
         dataframe.to_csv('output.csv', header=header_list, index=False)
@@ -58,4 +64,8 @@ if __name__ == "__main__":
 
 print("Code ran for:", time.time() - start_time, "seconds.")
 
+# Running a shell command to upload to Cloud Storage
 subprocess.run(["gsutil cp *.csv " + uri], shell=True)
+
+# Running the function from bq.update.py
+bigqueryupload()
